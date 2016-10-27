fc660c00d4f74c32e4a7475fed46ad5a614fd75f
Adam Stokes <battlemidget@users.noreply.github.com>
2016-10-27 09:57:15 -0400

    formatting
    Signed-off-by: Adam Stokes <battlemidget@users.noreply.github.com>

--- lib/dokuwiki.pm
+++ lib/dokuwiki.pm
@@ -8,14 +8,14 @@ has ua => sub { Mojo::UserAgent->new };
 
 sub get_download_page {
     my $self = shift;
-    return $self->ua->get('https://download.dokuwiki.org' => {Accept => '*/*'})
-      ->res->body;
+    return $self->ua->get('https://download.dokuwiki.org' => {Accept => '*/*'})->res->body;
 }
 
 sub get_stable_version {
     my $self = shift;
     my $dom  = Mojo::DOM->new($self->get_download_page);
     my @urls = $dom->find('a')->map('attr')->each;
+
     # Process each hash
     foreach my $url (@urls) {
         say $url->{href};
